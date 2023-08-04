3ccaa280133a1fb7e8852b1f5fe434c75cec2ae0
Amit Kumar <amitkm5364@gmail.com>
2023-08-04 12:21:31 +0530

    initial commit

---
+++ hello.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+# Create a flask Instance
+app = Flask(__name__)
+
+# Create a route decorator
+@app.route('/')
+def index():
+    return render_template("index.html")
+# local host:500/user/ravi
+@app.route('/user/<name>')
+def user(name):
+    return f"<h1>Hello {name}</h1>"
+
+# create Custom Error Pages
+
+#Invalid ULR
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
+# internal Server Error
+@app.errorhandler(500)
+def server_error(e):
+    return render_template("500.html")
+
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
