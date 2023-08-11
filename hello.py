fec469f64c5ee996451457966d9df070506beff9
Amit Kumar <amitkm5364@gmail.com>
2023-08-11 12:02:20 +0530

    Written code for WTF and FLASH

--- hello.py
+++ hello.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "This is my super secret key"
+
+# create a form class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # Create a route decorator
 @app.route('/')
@@ -24,6 +33,20 @@ def page_not_found(e):
 def server_error(e):
     return render_template("500.html")
 
+@app.route("/name", methods=["GET", "POST"])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate form
+    if form.validate_on_submit():
+        name = form.name.data
+        print(form.submit.type)
+        form.name.data = ''
+        flash("From submited successfully!")
+        
+    return render_template('name.html',
+                           name = name,
+                           form = form)
 
 
 
